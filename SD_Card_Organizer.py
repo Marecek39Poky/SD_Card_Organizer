1ccb6a6da2d9a2909f1f1e9a4bdcd78c17b6879d
MarecekPoky <marecekpokorny39+github@gmail.com>
2026-04-05 20:25:09 +0200

    Added comments and cleaned the code.

--- SD_Card_Organizer.py
+++ SD_Card_Organizer.py
@@ -1,15 +1,19 @@
+# Imports some needed utilities
 import os
 import shutil
 from datetime import datetime
 from pathlib import Path
 
+# Sets the current date to a variable
 date_today = datetime.now().strftime("%Y-%m-%d")
 
 # Asks the user which directory to organize
-input_dir_to_organize = input("Type which directory to organize: ")
+input_dir_to_organize = input("Which directory would you like to organize?")
 
+# Cleans the user input
 dir_to_organize = input_dir_to_organize.replace("\\", "/").replace('"', "").strip()
 
+# Verification of the directory to organize
 print("📁 Directory:", dir_to_organize)
 
 dir_contents = os.listdir(dir_to_organize)
@@ -19,8 +23,9 @@ print(dir_contents)
 
 confirm_directory = input("Is this the right directory? (Y/N): ")
 
-new_folder_name_from_user = input("Type name for the new organized directory: ")
+new_folder_name_from_user = input("Create a name for the new organized directory: ")
 
+# Creates new directories
 desktop_dir = Path.home() / "Desktop"
 new_folder_name = f"{date_today} {new_folder_name_from_user}"
 new_folder_path = desktop_dir / new_folder_name
@@ -30,35 +35,37 @@ new_folder_path = desktop_dir / new_folder_name
 (new_folder_path / "Videos").mkdir(parents=False, exist_ok=False)
 (new_folder_path / "Videos/Shots").mkdir(parents=False, exist_ok=False)
 (new_folder_path / "Videos/Proxy").mkdir(parents=False, exist_ok=False)
-print(f"Sucessfully creted folders in {new_folder_path}")
+print(f"Successfully created folders in {new_folder_path}")
 
+# Functions to copy from different manufacturers structures
 def sony_sdcard_copy():
-    path_shots = Path(dir_to_organize) / "PRIVATE/M4ROOT/CLIP"
-    path_proxy = Path(dir_to_organize) / "PRIVATE/M4ROOT/SUB"
-    path_photos = Path(dir_to_organize) / "DCIM"
+    sony_path_shots = Path(dir_to_organize) / "PRIVATE/M4ROOT/CLIP"
+    sony_path_proxy = Path(dir_to_organize) / "PRIVATE/M4ROOT/SUB"
+    sony_path_photos = Path(dir_to_organize) / "DCIM"
 
-    if path_shots.exists():
+    if sony_path_shots.exists():
         print("Copying original shots...")
-        for file in path_shots.glob("*.MP4"):
+        for file in sony_path_shots.glob("*.MP4"):
             shutil.copy2(file, new_folder_path / "Videos/Shots" / file.name)
-            print(f" OK: {file.name}")
+            print(f" Success: {file.name}")
     else:
         print("No original shots found.")
-    if path_proxy.exists():
+    if sony_path_proxy.exists():
         print("Copying proxy shots...")
-        for file in path_proxy.glob("*.MP4"):
+        for file in sony_path_proxy.glob("*.MP4"):
             shutil.copy2(file, new_folder_path / "Videos/Proxy" / file.name)
-            print(f" OK: {file.name}")
+            print(f" Success: {file.name}")
     else:
         print("No proxy shots found.")
-    if path_photos.exists():
-        print("Copying raw photos...")
-        for photo in path_photos.rglob("*.ARW"):
+    if sony_path_photos.exists():
+        print("Copying RAW photos...")
+        for photo in sony_path_photos.rglob("*.ARW"):
             shutil.copy2(photo, new_folder_path / "Photos" / photo.name)
-            print(f" OK: {photo.name}")
+            print(f" Success: {photo.name}")
     else:
-        print("No raw photos found.")
+        print("No RAW photos found.")
 
+# Calls functions
 if confirm_directory.lower() == "y":
     print("Copying starting...")
     sony_sdcard_copy()
